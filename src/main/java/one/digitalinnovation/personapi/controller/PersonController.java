d5ed39d68e3a21f8fdeccb69f2142e0aaa0d6568
juhcarvalho <jucarvalho@outlook.com.br>
2025-09-01 17:40:29 -0300

    Atualização da rota

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -14,7 +14,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/people")
+@RequestMapping("/people")
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
 
